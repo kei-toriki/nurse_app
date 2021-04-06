5b35c19db734d6c10bd2bb593af70121caddd38b
kei-toriki <sakanayama1954@icloud.com>
2021-04-06 10:19:35 +0900

    bootstrapの導入

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+const webpack = require('webpack')
+  environment.plugins.append(
+    'Provide',
+    new webpack.ProvidePlugin({
+      $: 'jquery/src/jquery',
+      jQuery: 'jquery/src/jquery',
+      Popper: ['popper.js', 'default']
+    })
+  )
