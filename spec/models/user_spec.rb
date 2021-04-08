fb6e0003d192a9adff57458eeb4016a988f9cb66
kei-toriki <sakanayama1954@icloud.com>
2021-04-08 17:08:55 +0900

    userモデルテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,13 @@ RSpec.describe User, type: :model do
       it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
         expect(@user).to be_valid
       end
+      it 'imageが空でも保存できること' do
+        @user.image = nil
+        expect(@user).to be_valid
+      end
     end
+
+
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
