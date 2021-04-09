c348b5bd672e8f1b02548b173aea1c834212a7aa
kei-toriki <sakanayama1954@icloud.com>
2021-04-09 19:05:26 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: "questions#index"
   resources :questions do
     resources :answers, only: [:new, :create]
+    collection do
+      get 'search'
+    end
   end
 end
