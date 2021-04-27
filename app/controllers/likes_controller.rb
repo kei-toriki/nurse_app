377e603b33e4a4b5bc3b9ade2e0764561aa07b30
kei-toriki <sakanayama1954@icloud.com>
2021-04-27 11:45:43 +0900

    いいね機能修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+
   def create
     @like = Like.create(user_id: current_user.id, question_id: params[:question_id])
     @question = Question.find_by(id: params[:question_id])
